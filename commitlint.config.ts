09ea29a34f82631a6b60409616b7858293d2c043
RYGrit <ry_grit@sina.com>
2024-01-28 21:48:40 +0800

    chore: config commitlint

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,3 +1,26 @@
-module.exports = {
+import type { UserConfig } from "@commitlint/types";
+
+const config: UserConfig = {
   extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "chore",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+      ],
+    ],
+  },
 };
+
+module.exports = config;
